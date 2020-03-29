63c7640330f5dd0b1fb12d5757f357184c9e64ef
AAAA <dev@onerbs.com>
2020-03-29 11:52:52 -0600

    initial

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Tasky from './Tasky'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Tasky/>
+  </React.StrictMode>,
+  document.getElementById('Tasky')
+)
