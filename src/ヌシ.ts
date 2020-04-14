c497aa57b55166106d29026784af76326d3fedfd
AAAA <dev@onerbs.com>
2020-04-14 03:28:07 -0500

    add theme

---
+++ src/ヌシ.ts
@@ -0,0 +1,50 @@
+export const ヌシ = {
+  breakpoints: ['360px', '480px', '1024px', '1280px'],
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64
+  ],
+  colors: {
+    primary: 'palevioletred',
+    fore: '#efefef',
+    back: '#000',
+    modal: '#222',
+  },
+  space: [
+    0, 4, 8, 16, 32, 64, 128, 256
+  ],
+  fonts: {
+    body: 'Inter, sans-serif',
+    heading: 'Inter, sans-serif',
+    monospace: 'Fira Code, monospace',
+  },
+  fontWeights: {
+    light: 200,
+    body: 400,
+    bold: 600,
+    heading: 800,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
+  shadows: {
+    default: '0 0 3px 4px rgba(0, 0, 0, 0.4)',
+    large: '0 0 4px 6px rgba(0, 0, 0, 0.55)'
+  },
+  radii: {
+    default: '3px',
+    plus: '5px',
+    extra: '8px',
+  },
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'primary',
+    },
+    outline: {
+      color: 'primary',
+      bg: 'transparent',
+      boxShadow: 'inset 0 0 0 2px'
+    },
+  },
+}
