df41758bc8595cdd42c77a4182044a8fbd27d0d1
AAAA <dev@onerbs.com>
2020-04-15 04:01:22 -0500

    +customizable

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,21 +1,53 @@
 import React from 'react'
 
-import { Flex } from 'rebass/styled-components'
-import { Text } from 'rebass'
+import { Box, Flex, Text } from 'rebass'
 import { Check, X } from 'react-feather'
 
 export default ({
-  color = 'primary',
   LeftIcon = X,
   leftAction = () => {},
+  showLeftIcon = true,
+  leftColor = '',
   RightIcon = Check,
-  rightAction = () => {}
-}) =>
-<Flex p={3} className='Controls' justifyContent='space-between'>
-  <Text as='span' color={color} onClick={leftAction} >
-    <LeftIcon cursor='pointer'/>
-  </Text>
-  <Text as='span' color={color} onClick={rightAction} >
-    <RightIcon cursor='pointer'/>
-  </Text>
-</Flex>
+  rightAction = () => {},
+  showRightIcon = true,
+  rightColor = '',
+  px, py, pad = [3],
+  color = 'primary'
+}: {
+  LeftIcon?: React.ComponentType<any>
+  leftAction?: () => void
+  showLeftIcon?: boolean,
+  leftColor?: string,
+  RightIcon?: React.ComponentType<any>
+  rightAction?: () => void
+  showRightIcon?: boolean,
+  rightColor?: string,
+  px?: number | number[],
+  py?: number | number[],
+  pad?: number | number[],
+  color?: string
+}) => {
+  return (
+    <Flex px={px ? px : pad} py={py ? py : pad}
+      justifyContent='space-between'>
+      <Box width={1/2} sx={{ textAlign: 'left' }}>
+        {showLeftIcon && <Text as={'span'}
+          color={leftColor ? leftColor : color}
+          onClick={leftAction}
+          >
+          <LeftIcon cursor='pointer'/>
+        </Text>}
+      </Box>
+
+      <Box width={1/2} sx={{ textAlign: 'right' }}>
+        {showRightIcon && <Text as={'span'}
+          color={rightColor ? rightColor : color}
+          onClick={rightAction}
+          >
+          <RightIcon cursor='pointer'/>
+        </Text>}
+      </Box>
+    </Flex>
+  )
+}
